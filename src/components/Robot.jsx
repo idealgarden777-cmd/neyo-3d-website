f621f49275a19405dac6a5a47ae57c9e2bf94d84
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-09 01:10:55 +0500

    Update Robot.jsx

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,21 +1,31 @@
+import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Robot() {
   const robotRef = useRef();
   const { scene } = useGLTF("/models/robot.glb");
+
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material = child.material.clone();
+
+      child.material.color = new THREE.Color("#d9d9d9");
+      child.material.metalness = 0.75;
+      child.material.roughness = 0.25;
+
+      child.material.needsUpdate = true;
+    }
+  });
 
   useFrame((state, delta) => {
     if (!robotRef.current) return;
 
     robotRef.current.rotation.y += delta * 0.15;
 
-    robotRef.current.rotation.x =
-      state.pointer.y * 0.05;
-
-    robotRef.current.rotation.z =
-      -state.pointer.x * 0.03;
+    robotRef.current.rotation.x = state.pointer.y * 0.04;
+    robotRef.current.rotation.z = -state.pointer.x * 0.025;
   });
 
   return (
