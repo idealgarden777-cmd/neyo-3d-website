81cf88ebbfb6354148181dc06e367069904227e0
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-08 23:38:43 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment } from "@react-three/drei";
-import Robot from "./components/Robot";
+import Scene from "./components/Scene";
 
 function App() {
   return (
-    <div className="app" style={{ width: "100vw", height: "100vh" }}>
-      <Canvas camera={{ position: [0, 1.5, 5], fov: 45 }}>
-        <ambientLight intensity={1} />
-
-        <Environment preset="studio" />
-
-        <Robot />
-
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Scene />
+    </main>
   );
 }
 
