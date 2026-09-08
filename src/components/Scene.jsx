a3fd08e4b28d9635425af6848683037f8742ba18
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-09 00:06:43 +0500

    Update Scene.jsx

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,49 +1,20 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment } from "@react-three/drei";
-import Robot from "./Robot";
+import { OrbitControls } from "@react-three/drei";
 
 export default function Scene() {
   return (
     <Canvas
-      camera={{
-        position: [0, 1.2, 5],
-        fov: 40,
-      }}
-      gl={{
-        antialias: true,
-      }}
-      shadows
+      camera={{ position: [0, 0, 5], fov: 45 }}
+      style={{ width: "100%", height: "100vh" }}
     >
-      <ambientLight intensity={0.35} />
+      <ambientLight intensity={2} />
 
-      <directionalLight
-        position={[4, 6, 4]}
-        intensity={2.5}
-        castShadow
-      />
+      <mesh>
+        <boxGeometry args={[1.5, 1.5, 1.5]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
 
-      <pointLight
-        position={[-4, 2, 3]}
-        intensity={25}
-        distance={10}
-      />
-
-      <pointLight
-        position={[4, 1, -2]}
-        intensity={15}
-        distance={8}
-      />
-
-      <Environment preset="city" />
-
-      <Robot />
-
-      <OrbitControls
-        enablePan={false}
-        minDistance={3}
-        maxDistance={7}
-        target={[0, 0, 0]}
-      />
+      <OrbitControls />
     </Canvas>
   );
 }
