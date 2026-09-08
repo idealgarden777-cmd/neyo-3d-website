a58e46788a1b36d10db0942fd3f18e57869875da
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-08 23:45:05 +0500

    Update Robot.jsx

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,14 +1,23 @@
+import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Robot() {
   const { scene } = useGLTF("/models/robot.glb");
+  const robotRef = useRef();
+
+  useFrame((state, delta) => {
+    if (robotRef.current) {
+      robotRef.current.rotation.y += delta * 0.25;
+    }
+  });
 
   return (
     <primitive
+      ref={robotRef}
       object={scene}
       scale={1.8}
       position={[0, -1.4, 0]}
-      rotation={[0, 0, 0]}
     />
   );
 }
