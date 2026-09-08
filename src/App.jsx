327cd8c6a338100c63c8858e7a27a159b4e6630b
idealgarden777-cmd <idealgarden777@gmail.com>
2026-09-08 23:33:10 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1>Neyo 3D Website</h1>;
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+
+function App() {
+  return (
+    <div className="app">
+      <Canvas camera={{ position: [0, 1.5, 5], fov: 45 }}>
+        <ambientLight intensity={1} />
+
+        <Environment preset="studio" />
+
+        <OrbitControls />
+
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
+
+export default App;
